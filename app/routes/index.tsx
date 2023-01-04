427349b56a539fee76f102db56813e67d4157593
MaymoonaAlBoloshi <maymoona.al.boloshi@gmail.com>
2023-01-04 21:15:51 +0400

    homepage

---
+++ app/routes/index.tsx
@@ -0,0 +1,53 @@
+import { Link } from "@remix-run/react";
+
+import { useOptionalUser } from "~/utils";
+
+export default function Index() {
+  const user = useOptionalUser();
+  return (
+    <main className="relative min-h-screen bg-white bg-gradient-to-l from-purple-400 via-white to-pink-600 sm:flex sm:items-center sm:justify-center">
+      <div className="relative sm:pb-16 sm:pt-8">
+        <div className="mx-auto max-w-7xl sm:px-6 lg:px-8 ">
+          <div className="relative bg-white  shadow-xl sm:rounded-2xl">
+            <div className="relative px-4 pt-16 pb-8 sm:px-6 sm:pt-24 sm:pb-14 lg:px-8 lg:pb-20 lg:pt-32">
+              <h1 className="text-center text-6xl font-extrabold tracking-tight sm:text-8xl lg:text-9xl">
+                <span className="block bg-gradient-to-r from-purple-400 to-pink-600 bg-clip-text text-8xl font-extrabold uppercase text-transparent drop-shadow-md">
+                  Fly Mail
+                </span>
+              </h1>
+              <p className="bold mx-auto mt-6 max-w-lg text-center text-xl text-2xl text-pink-300 sm:max-w-3xl">
+                Fly Mail is a simple, Email markiting tool that helps you send
+                branded emails to your customers.
+              </p>
+              <div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
+                {user ? (
+                  <Link
+                    to="/notes"
+                    className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-yellow-700 shadow-sm hover:bg-yellow-50 sm:px-8"
+                  >
+                    View templates for {user.email}
+                  </Link>
+                ) : (
+                  <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
+                    <Link
+                      to="/join"
+                      className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-pink-600 shadow-sm hover:bg-yellow-50 sm:px-8"
+                    >
+                      Sign up
+                    </Link>
+                    <Link
+                      to="/login"
+                      className="flex items-center justify-center rounded-md bg-gradient-to-r from-purple-400 to-pink-600 px-4 py-3 font-medium text-white hover:bg-yellow-600"
+                    >
+                      Log In
+                    </Link>
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
