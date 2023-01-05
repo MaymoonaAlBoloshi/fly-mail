ad9d453cb12ce471922a47e0259ae9efd84fed02
MaymoonaAlBoloshi <maymoona.al.boloshi@gmail.com>
2023-01-05 09:59:59 +0400

    layout style updated

--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -18,15 +18,15 @@ export default function NotesPage() {
 
   return (
     <div className="flex h-full min-h-screen flex-col">
-      <header className="flex items-center justify-between bg-slate-800 p-4 text-white">
+      <header className="flex items-center justify-between bg-purple-900 p-4 text-white">
         <h1 className="text-3xl font-bold">
-          <Link to=".">Notes</Link>
+          <Link to=".">Fly Mail</Link>
         </h1>
         <p>{user.email}</p>
         <Form action="/logout" method="post">
           <button
             type="submit"
-            className="rounded bg-slate-600 py-2 px-4 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
+            className="rounded bg-pink-600 py-2 px-4 text-pink-100 hover:bg-pink-500 active:bg-pink-600"
           >
             Logout
           </button>
@@ -34,31 +34,12 @@ export default function NotesPage() {
       </header>
 
       <main className="flex h-full bg-white">
-        <div className="h-full w-80 border-r bg-gray-50">
-          <Link to="new" className="block p-4 text-xl text-blue-500">
-            + New Note
+        <div className="h-full w-80 border-r bg-purple-50">
+          <Link to="new" className="block p-4 text-xl text-purple-500">
+            + Add new template
           </Link>
-
           <hr />
-
-          {data.noteListItems.length === 0 ? (
-            <p className="p-4">No notes yet</p>
-          ) : (
-            <ol>
-              {data.noteListItems.map((note) => (
-                <li key={note.id}>
-                  <NavLink
-                    className={({ isActive }) =>
-                      `block border-b p-4 text-xl ${isActive ? "bg-white" : ""}`
-                    }
-                    to={note.id}
-                  >
-                    📝 {note.title}
-                  </NavLink>
-                </li>
-              ))}
-            </ol>
-          )}
+          <p className="p-4">No options yet</p>
         </div>
 
         <div className="flex-1 p-6">
--- app/routes/notes/index.tsx
+++ app/routes/notes/index.tsx
@@ -1,8 +1,6 @@
 import { Link } from "@remix-run/react";
 
 export default function NoteIndexPage() {
-  // create a list of html templates for emails strings in an array
-
   const templates = [
     {
       id: 1,
@@ -20,26 +18,44 @@ export default function NoteIndexPage() {
       id: 3,
       name: "Template 3",
       description: "This is a template",
-      html: "<h1>Template 3</h1>",
+      html: `<h1 class="text-2xl text-pink-600">Template 3</h1>`,
+    },
+    {
+      id: 4,
+      name: "Template 4",
+      description: "This is a template",
+      html: "<h1>Template 4</h1>",
+    },
+    {
+      id: 5,
+      name: "Template 5",
+      description: "This is a template",
+      html: "<h1>Template 5</h1>",
     },
   ];
 
   return (
-    <div className="flex flex-col gap-12">
+    <div className="flex h-full flex-row flex-wrap justify-center gap-12">
       {templates.map((template) => {
         return (
           <div
             key={template.id}
-            className="rounded border-2 border-red-300 p-2"
+            className="flex h-2/4 w-1/3 flex-col rounded p-2 shadow-md"
           >
             <h1 className="text-2xl text-pink-600">{template.name}</h1>
             <p className="text-sm text-gray-600">{template.description}</p>
-            <section className="mt-2 shadow-sm">
-              <div dangerouslySetInnerHTML={{ __html: template.html }} />
+            <section className="mt-2 flex-1 shadow-sm">
+              {/* add an image instead of how it looks like */}
+              <div className="scale-50 w-full"   dangerouslySetInnerHTML={{ __html: template.html }} />
             </section>
+            <button className="m-2 mt-2 rounded bg-pink-600 p-2 text-white hover:animate-pulse hover:bg-pink-500 hover:shadow-md">
+              use template
+            </button>
           </div>
         );
       })}
     </div>
   );
 }
+// you need this to edit html
+//https://www.npmjs.com/package/react-contenteditable?activeTab=readme
